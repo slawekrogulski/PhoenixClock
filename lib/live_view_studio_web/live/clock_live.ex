e7b425af04300af6152d5e6623d1ec957665b771
Slawek Rogulski <slawek.rogulski@gmail.com>
2024-12-05 19:10:50 +0800

    force time zone to Asia/Singapore

--- lib/live_view_studio_web/live/clock_live.ex
+++ lib/live_view_studio_web/live/clock_live.ex
@@ -102,7 +102,7 @@ defmodule LiveViewStudioWeb.ClockLive do
 
   def handle_info(:tick, socket) do
     # IO.inspect(socket.assigns, label: "assigns")
-    local_timezone = Map.get(socket.assigns, :local_timezone, "UTC")
+    local_timezone = Map.get(socket.assigns, :local_timezone, "Asia/Singapore")
     current_datetime = Timex.now(local_timezone)
     #  local_time_zone(socket.assigns)
     # current_datetime = Timex.local()
