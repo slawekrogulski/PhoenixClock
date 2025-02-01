7f4cae69ce9a0eb80a2592be0b4287eb6a68ad40
Slawek Rogulski <slawek.rogulski@gmail.com>
2025-02-01 16:12:34 +0800

    version bumps and cleanup

--- mix.exs
+++ mix.exs
@@ -4,8 +4,8 @@ defmodule LiveViewStudio.MixProject do
   def project do
     [
       app: :live_view_studio,
-      version: "0.1.0",
-      elixir: "~> 1.18",
+      version: "0.2.0",
+      elixir: "~> 1.18.2",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
       aliases: aliases(),
@@ -32,30 +32,34 @@ defmodule LiveViewStudio.MixProject do
   # Type `mix help deps` for examples and options.
   defp deps do
     [
-      {:phoenix, "~> 1.7.19"},
-      # {:phoenix_ecto, "~> 4.4"},
-      # {:ecto_sql, "~> 3.6"},
-      # {:postgrex, ">= 0.0.0"},
-      {:phoenix_html, "~> 3.3"},
-      {:phoenix_live_reload, "~> 1.5.3", only: :dev},
-      {:phoenix_live_view, "~> 1.0.3"},
-      {:heroicons, "~> 0.5.3"},
-      {:floki, ">= 0.36.0", only: :test},
-      {:phoenix_live_dashboard, "~> 0.8.6"},
-      {:esbuild, "~> 0.8.2", runtime: Mix.env() == :dev},
-      {:tailwind, "~> 0.2.0", runtime: Mix.env() == :dev},
-      {:swoosh, "~> 1.3"},
-      {:finch, "~> 0.13"},
-      {:telemetry_metrics, "~> 0.6"},
-      {:telemetry_poller, "~> 1.0"},
-      {:gettext, "~> 0.21.0"},
-      {:jason, "~> 1.2"},
-      {:plug_cowboy, "~> 2.5"},
-      {:bcrypt_elixir, "~> 3.0"},
-      {:faker, "~> 0.17.0"},
-      {:number, "~> 1.0"},
-      {:timex, "~> 3.7"},
-      {:typed_struct, "~> 0.3.0"}
+      {:phoenix               , "~> 1.7.19"                           },
+      {:phoenix_html          , "~> 4.1"                              },
+      {:phoenix_live_reload   , "~> 1.2",   only: :dev                },
+      {:phoenix_live_view     , "~> 1.0.0"                            },
+      {:heroicons,
+       github: "tailwindlabs/heroicons",
+       tag: "v2.1.1",
+       sparse: "optimized",
+       app: false,
+       compile: false,
+       depth: 1},
+      # {:heroicons             , "~> 0.5.6"                            },
+      {:floki                 , ">= 0.36.0", only: :test              },
+      {:phoenix_live_dashboard, "~> 0.8.6"                            },
+      {:esbuild               , "~> 0.8",   runtime: Mix.env() == :dev},
+      {:tailwind              , "~> 0.2.0", runtime: Mix.env() == :dev},
+      {:swoosh                , "~> 1.17.8"                           },
+      {:finch                 , "~> 0.19"                             },
+      {:telemetry_metrics     , "~> 1.1.0"                            },
+      {:telemetry_poller      , "~> 1.1.0"                            },
+      {:gettext               , "~> 0.26.2"                           },
+      {:jason                 , "~> 1.4.4"                            },
+      {:plug_cowboy           , "~> 2.7.2"                            },
+      {:bcrypt_elixir         , "~> 3.2.0"                            },
+      {:faker                 , "~> 0.18.0"                           },
+      {:number                , "~> 1.0.5"                            },
+      {:timex                 , "~> 3.7.11"                           },
+      {:typed_struct          , "~> 0.3.0"                            }
     ]
   end
 
@@ -68,20 +72,28 @@ defmodule LiveViewStudio.MixProject do
   defp aliases do
     [
       setup: ["deps.get", "assets.setup", "assets.build"],
-      test: ["ecto.create --quiet", "ecto.migrate --quiet", "test"],
-      "assets.setup": [
-        "tailwind.install --if-missing",
-        "esbuild.install --if-missing"
-      ],
-      "assets.build": [
-        "tailwind default",
-        "esbuild default"
-      ],
+      "assets.setup": ["tailwind.install --if-missing", "esbuild.install --if-missing"],
+      "assets.build": ["tailwind my_app", "esbuild my_app"],
       "assets.deploy": [
-        "tailwind default --minify",
-        "esbuild default --minify",
+        "tailwind my_app --minify",
+        "esbuild my_app --minify",
         "phx.digest"
       ]
+      # setup: ["deps.get", "assets.setup", "assets.build"],
+      # test: ["ecto.create --quiet", "ecto.migrate --quiet", "test"],
+      # "assets.setup": [
+      #   "tailwind.install --if-missing",
+      #   "esbuild.install --if-missing"
+      # ],
+      # "assets.build": [
+      #   "tailwind default",
+      #   "esbuild default"
+      # ],
+      # "assets.deploy": [
+      #   "tailwind default --minify",
+      #   "esbuild default --minify",
+      #   "phx.digest"
+      # ]
     ]
   end
 end
