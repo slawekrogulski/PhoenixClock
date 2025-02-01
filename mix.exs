6bdcef07ed79bff8ead3361815c7dcd07015fa81
Slawek Rogulski <slawek.rogulski@gmail.com>
2025-02-01 14:36:55 +0800

    update dependencies and elixir

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule LiveViewStudio.MixProject do
     [
       app: :live_view_studio,
       version: "0.1.0",
-      elixir: "~> 1.14",
+      elixir: "~> 1.18",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
       aliases: aliases(),
@@ -32,17 +32,17 @@ defmodule LiveViewStudio.MixProject do
   # Type `mix help deps` for examples and options.
   defp deps do
     [
-      {:phoenix, "~> 1.7.2"},
-      {:phoenix_ecto, "~> 4.4"},
-      {:ecto_sql, "~> 3.6"},
-      {:postgrex, ">= 0.0.0"},
+      {:phoenix, "~> 1.7.19"},
+      # {:phoenix_ecto, "~> 4.4"},
+      # {:ecto_sql, "~> 3.6"},
+      # {:postgrex, ">= 0.0.0"},
       {:phoenix_html, "~> 3.3"},
-      {:phoenix_live_reload, "~> 1.2", only: :dev},
-      {:phoenix_live_view, "~> 0.18.18"},
+      {:phoenix_live_reload, "~> 1.5.3", only: :dev},
+      {:phoenix_live_view, "~> 1.0.3"},
       {:heroicons, "~> 0.5.3"},
-      {:floki, ">= 0.30.0", only: :test},
-      {:phoenix_live_dashboard, "~> 0.7.2"},
-      {:esbuild, "~> 0.7", runtime: Mix.env() == :dev},
+      {:floki, ">= 0.36.0", only: :test},
+      {:phoenix_live_dashboard, "~> 0.8.6"},
+      {:esbuild, "~> 0.8.2", runtime: Mix.env() == :dev},
       {:tailwind, "~> 0.2.0", runtime: Mix.env() == :dev},
       {:swoosh, "~> 1.3"},
       {:finch, "~> 0.13"},
@@ -67,9 +67,7 @@ defmodule LiveViewStudio.MixProject do
   # See the documentation for `Mix` for more info on aliases.
   defp aliases do
     [
-      setup: ["deps.get", "ecto.setup", "assets.setup", "assets.build"],
-      "ecto.setup": ["ecto.create", "ecto.migrate", "run priv/repo/seeds.exs"],
-      "ecto.reset": ["ecto.drop", "ecto.setup"],
+      setup: ["deps.get", "assets.setup", "assets.build"],
       test: ["ecto.create --quiet", "ecto.migrate --quiet", "test"],
       "assets.setup": [
         "tailwind.install --if-missing",
